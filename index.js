3e0a50e8d5c43cb84576ef0eb876adb174f64eb4
ardhicaturk <ardhicaturk3@gmail.com>
2019-01-23 23:28:54 +0700

    1st commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(8081);
 const redis = require('socket.io-redis');
 
 //===================================== HTTP Event Handler ========================================//
@@ -10,21 +10,85 @@ app.use(function (req, res, next) {
     next();
 });
 app.get('/', function (req, res, next) {
-    res.status(200).send('<h1>Hello world</h1>');
+    res.status(200).sendFile(__dirname + '/webpage/index.html');;
 });
 
 //====================================== Socket IO Event Handler ====================================//
-
+var nodeList = new Object();
+nodeList.ID = new Array();
+nodeList.name = new Array();
+nodeList.RSSI = new Array();
+var inChange = false;
+async function checkMember(ids, names) {
+    var a = nodeList.name.indexOf(names);
+    if (a > -1) {
+        inChange = true;
+        if (nodeList.ID[a] != 0) {
+            io.sockets.connected[nodeList.ID[a]].disconnect(true);
+        }
+        nodeList.ID[a] = ids;
+        console.log(names + " is connected");
+    } else {
+        console.log(names + " is connected");
+        nodeList.name.push(names);
+        nodeList.ID.push(ids);
+        nodeList.RSSI.push(0);
+        
+    }
+}
+function translateRSSI(num){
+    if(num >= -30){
+        return "Amazing";
+    } else if (num >= -70){
+        return "Good";
+    } else if (num >= -90){
+        return "Bad";
+    } else {
+        return "Poor";
+    }
+}
+function saveInput(dataType, names, dta){
+    var buf = nodeList.name.indexOf(names);
+    if(buf > -1){
+        switch(dataType){
+            case "RSSI":
+                nodeList.RSSI[buf] = dta;
+                //console.log("RSSI " + nodeList.name[buf] + ": " + translateRSSI(nodeList.RSSI[buf]));
+            break;
+            case "param1":
+            break;
+            default:
+            break;
+        }
+    }
+    
+}
 io.on('connection', function (socket) {
-    console.log('a user connected: ' + socket.id);
-    socket.send("Your ID: " + socket.id);
-    io.sockets.connected[socket.id].emit("greeting", "Howdy, User 1!");
+    //console.log('a user connected: ' + socket.id);
+    //socket.send("Your ID: " + socket.id);
+    io.sockets.connected[socket.id].emit("ID", socket.id);
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+        var a = nodeList.ID.indexOf(socket.id);
+        console.log(nodeList.name[a] + ' is disconnected');
+        nodeList.ID[a] = 0;
+
+    });
+    socket.on('greeting', function (data) {
+        console.log(data);
+    });
+    socket.on('nameNode', function (data) {
+        var a = data.split(","); // 0: name, 1: ID
+        checkMember(a[1], a[0]);
+    });
+    socket.on('RSSI', function (data) {
+        var a = data.split(","); // 0: name, 1: RSSI
+        //checkMember(a[1], a[0]);
+        saveInput("RSSI", a[0], a[1]);
+        //console.log(data);
     });
 });
 
-var port = process.env.PORT || 8099;
+var port = process.env.PORT || 8080;
 http.listen(port, function () {
     console.log('listening on *:' + port);
 });
