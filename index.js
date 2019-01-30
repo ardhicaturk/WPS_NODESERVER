8ea3604e31a4af282287a30f980b8583e03dfb63
ardhicaturk <ardhicaturk3@gmail.com>
2019-01-30 10:35:27 +0700

    mayor update 2

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const app = require('express')();
 var port = process.env.PORT || 8089;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const redis = require('socket.io-redis');
+var Sequelize = require('sequelize');
+var pg = require('pg');
+pg.defaults.ssl = true;
+//const redis = require('socket.io-redis');
 var loginPage = require('./login');
+var User = require('./login').modelUser();
 //===================================== HTTP Event Handler ========================================//
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -17,15 +21,20 @@ app.get('/', function (req, res, next) {
 */
 loginPage.init(app);
 app.get('/socket', function (req, res, next) {
-    res.status(200).sendFile(__dirname + '/webpage/socket.html');;
+    res.status(200).sendFile(__dirname + '/webpage/socket.html');
 });
 app.get('/js/socket.io.js', function (req, res, next) {
-    res.status(200).sendFile(__dirname + '/webpage/js/socket.io.js');;
+    res.status(200).sendFile(__dirname + '/webpage/js/socket.io.js');
 });
 app.get('/js/jquery-3.1.1.js', function (req, res, next) {
-    res.status(200).sendFile(__dirname + '/webpage/js/jquery-3.1.1.js');;
+    res.status(200).sendFile(__dirname + '/webpage/js/jquery-3.1.1.js');
 });
-
+app.get('/css/bootstrap.min.css', function (req, res, next) {
+    res.status(200).sendFile(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css');
+});
+app.get('/js/bootstrap.min.js', function (req, res, next) {
+    res.status(200).sendFile(__dirname + '/node_modules/bootstrap/dist/js/bootstrap.min.js');
+});
 //====================================== Socket IO Event Handler ====================================//
 var nodeList = new Object();
 nodeList.ID = new Array();
@@ -33,9 +42,30 @@ nodeList.name = new Array();
 nodeList.RSSI = new Array();
 nodeList.SID = new Array();
 
+/*
+Additional nodeList:
+-Status
+-RSSI
+-Tegangan
+-Arus
+-Sakelar
+-Kondisi
+*/
 var clientList = new Object();
 clientList.user = new Array();
 clientList.ID = new Array();
+clientList.node = new Array();
+clientList.node.SID = new Array();
+clientList.node.nameNode = new Array();
+/*
+var a = {SID: "val", nodeName: "val"};
+node.push(a)
+#clientList.node struktur
+node
+-SID
+-nodeName
+
+*/
 var inChange = false;
 async function checkMember(ids, SID, names) {
     var a = nodeList.name.indexOf(SID);
@@ -95,7 +125,7 @@ io.on('connection', function (socket) {
             nodeList.ID[a] = 0;
         } else {
             var b = clientList.ID.indexOf(socket.id);
-            console.log('web client: '+clientList.user[b]+' is disconnected');
+            console.log('web client: ' + clientList.user[b] + ' is disconnected');
         }
 
     });
@@ -105,7 +135,14 @@ io.on('connection', function (socket) {
     //========================================== NODE HANDLER ======================================//
     socket.on('nameNode', function (data) {
         var a = data.split(","); // 0: name, 1: ID, 2: SID
-        checkMember(a[1], a[2], a[0]);
+        var i = clientList.node.findIndex(x => x.SID == a[2]);
+        var name;
+        if(i > -1){
+            name = clientList.node[i].nameNode;
+        } else {
+            name = a[0];
+        }
+        checkMember(a[1], a[2], name);
     });
     socket.on('RSSI', function (data) {
         var a = data.split(","); // 0: SID, 1: RSSI
@@ -123,11 +160,45 @@ io.on('connection', function (socket) {
             clientList.ID.push(a[1]);
             clientList.user.push(a[0]);
         }
-        io.sockets.connected[a[1]].emit("node", {num: 5, name: ["node1", "node2", "node3", "node4", "node5"]});
     });
 
+    socket.on("getNode", function(data){
+        var modul = loginPage.getNode(data);
+        modul.sync()
+        .then(() => {
+            //console.log('sync ok');
+            modul.findAll().then(users => {
+                //console.log("maman : " + JSON.stringify(users) + " \n" + users[0].SID);
+                var index = clientList.user.indexOf(data);
+                //console.log("ID CLIENT: " + clientList.ID[index]);
+                io.sockets.connected[clientList.ID[index]].emit("responseGetNode", users);
+                for(var i = 0; i < users.length; i++){
+                    var a = {SID: users[i].SID, nameNode: users[i].nameNode};
+                    var b = clientList.node.findIndex(x => x.SID == users[i].SID);
+                    if(b > -1){
+                        clientList.node[b].SID = users[i].SID;
+                        clientList.node[b].nameNode = users[i].nameNode;
+                    } else {
+                        clientList.node.push(a);
+                    }
+                }
+                console.log(JSON.stringify(clientList.node));
+            });
+        })
+        .catch(error => console.log('This error occured', error));
+        
+    });
 });
 
 http.listen(port, function () {
     console.log('listening on :' + port);
+    User.sync()
+        .then(() => {
+            //console.log('sync ok');
+            User.findAll().then(user => {
+                console.log("sync ok: " + user.length) +"users";
+            });
+        })
+        .catch(error => console.log('This error occured', error));
+
 });
--- login.js
+++ login.js
@@ -2,8 +2,98 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
+var pg = require('pg');
+pg.defaults.ssl = true;
+// var sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/auth-system', {
+//     logging: false
+// });
+var sequelize = new Sequelize('postgres://zjczluqbvhxxsh:090ac543674f2e44c99c8835073d324b7f05586928317f336fbe630732bf09a6@ec2-107-22-238-186.compute-1.amazonaws.com:5432/d8lre5n7ki3ob1', {
+    logging: false
+});
 var md5 = require('md5');
+
+var usr = sequelize.define('users', {
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    nama: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    alamat: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    nomorhp: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+function checkNodeInUser(username) {
+    var model = newUser(username);
+    model.sync()
+        .then(() => {
+            model.findAll().then(user => {
+                console.log("Jumlah node terdaftar " + username + ": " + user.length) + "node";
+            });
+            return user.length;
+        })
+        .catch(error => {
+            console.log(username + " belum memiliki node");
+            return 0;
+        });
+}
+
+function createNewNode(model, SID, nameNode, keterangan) {
+    model.create({
+            SID: SID,
+            nameNode: nameNode,
+            keterangan: keterangan
+        })
+        .then(user => {
+            //newUser(username);
+            return 1;
+        })
+        .catch(error => {
+            return 0;
+        });
+}
+
+function newUser(username) {
+    var modelUser = sequelize.define(username, {
+        SID: {
+            type: Sequelize.STRING,
+            unique: true,
+            allowNull: false
+        },
+        nameNode: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        keterangan: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    });
+    return modelUser;
+}
 module.exports = {
+    modelUser: function () {
+        return usr;
+    },
     init: function (app) {
         app.use(bodyParser.urlencoded({
             extended: true
@@ -34,24 +124,39 @@ module.exports = {
         app.get('/', sessionChecker, (req, res) => {
             res.redirect('/login');
         });
+        //=================================== sign up event ===============================//
         app.route('/signup')
             .get(sessionChecker, (req, res) => {
                 res.sendFile(__dirname + '/webpage/signup.html');
             })
             .post((req, res) => {
-                User.create({
-                        username: req.body.username,
-                        email: req.body.email,
-                        password: req.body.password
-                    })
-                    .then(user => {
-                        req.session.user = user.dataValues;
-                        res.redirect('/dashboard');
-                    })
-                    .catch(error => {
-                        res.redirect('/signup');
-                    });
+                usr.findOne({
+                    where: {
+                        username: req.body.username
+                    }
+                }).then(function (user) {
+                    res.redirect('/signup?auth=1');
+                }).catch((error) => {
+                    usr.create({
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: md5(req.body.password),
+                            nama: req.body.nama,
+                            alamat: req.body.alamat,
+                            nomorhp: req.body.nomorhp,
+                        })
+                        .then(user => {
+                            //newUser(username);
+                            res.redirect('/login?auth=1');
+                        })
+                        .catch(error => {
+                            console.log(error);
+                            res.redirect('/signup?auth=1');
+                        });
+                });
+
             });
+        //=================================== sign in event ===============================//
         app.route('/login')
             .get(sessionChecker, (req, res) => {
                 res.sendFile(__dirname + '/webpage/login.html');
@@ -59,36 +164,28 @@ module.exports = {
                 var username = req.body.username,
                     password = req.body.password;
                 password = md5(password);
-                bUser = username == "ardhi" ? true : false;
-                bPassword = password == md5("ardhi") ? true : false;
-                if (!bUser) {
-                    res.redirect('/login?auth=0');
-                    //res.cookie("auth", "0");
-                } else if (!bPassword) {
-                    res.redirect('/login?auth=0');
-                    //res.cookie("auth", "0");
-                } else {
-                    req.session.user = "ardhi";
-                    res.cookie("username", username);
-                    res.redirect('/dashboard');
-                }
-                /*
-                User.findOne({
+                usr.findOne({
                     where: {
                         username: username
                     }
                 }).then(function (user) {
+                    console.log("comp: " + password + " & " + user.get('password'));
+                    var comp = password == user.get('password') ? true : false;
                     if (!user) {
-                        res.redirect('/login');
-                    } else if (!user.validPassword(password)) {
-                        res.redirect('/login');
+                        res.redirect('/login?auth=0');
+                    } else if (!comp) {
+                        res.redirect('/login?auth=0');
                     } else {
                         req.session.user = user.dataValues;
+                        res.cookie("username", username);
                         res.redirect('/dashboard');
                     }
+                }).catch(error => {
+                    res.redirect('/login?auth=2');
                 });
-                */
+
             });
+        //=================================== dashboard event ===============================//
         app.get('/dashboard', (req, res) => {
             if (req.session.user && req.cookies.user_sid) {
                 res.sendFile(__dirname + '/webpage/dashboard.html');
@@ -96,6 +193,7 @@ module.exports = {
                 res.redirect('/login');
             }
         });
+        //=================================== logout event ====================================//
         app.get('/logout', (req, res) => {
             if (req.session.user && req.cookies.user_sid) {
                 res.clearCookie('user_sid');
@@ -104,7 +202,32 @@ module.exports = {
                 res.redirect('/login');
             }
         });
+        //=================================== daftar node event ================================//
+        app.route('/daftarnode').get((req, res) => {
+            if (req.session.user && req.cookies.user_sid) {
+                if (checkNodeInUser(req.cookies.username) == 0) {
+                    res.sendFile(__dirname + '/webpage/daftarnode.html');
+                    res.cookie("node", 0);
+                } else {
+                    res.sendFile(__dirname + '/webpage/daftarnode.html');
+                }
+            } else {
+                res.redirect('/login');
+            }
+        }).post((req, res) => {
+            var SID = req.body.sid,
+                namaNode = req.body.namanode,
+                ket = req.body.keterangan;
+                createNewNode(newUser(req.cookies.username), SID, namaNode, ket);
+                res.redirect('/daftarnode');
+        });
+    },
+    authdb: function () {
+        return sequelize;
+    },
+    getNode: function(username){
+        var modul = newUser(username);
+        return modul;
     }
-
     //etc
 }
